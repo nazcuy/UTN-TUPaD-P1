4df77e11cbc7af7f53ddd92ecde8cb1f72fb0f30
Nicolás Azcuy <nico.azcuy@gmail.com>
2025-04-15 19:23:46 -0300

    Subo ejercicio 1 terminado.

---
+++ 05 Funciones/Azcuy_TP-05.py
@@ -0,0 +1,9 @@
+"""1. Crear una función llamada imprimir_hola_mundo que imprima por
+pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
+programa principal."""
+#Funciones
+def imprimir_hola_mundo():
+    print("Hola Mundo!")
+
+#Programa Principal
+imprimir_hola_mundo()
