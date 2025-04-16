8984fc8b6fc593f18d2d3ec4c948a73bcf70af18
Nicolás Azcuy <nico.azcuy@gmail.com>
2025-04-15 23:06:40 -0300

    Subo ejercicio 8.

--- 05 Funciones/Azcuy_TP-05.py
+++ 05 Funciones/Azcuy_TP-05.py
@@ -105,3 +105,17 @@ a_usuario = int(input("Ingrese un número: "))
 b_usuario = int(input("Ingrese otro número: "))
 operaciones_basicas(a_usuario, b_usuario)
 """
+"""8. Crear una función llamada calcular_imc(peso, altura) que reciba el
+peso en kilogramos y la altura en metros, y devuelva el índice de
+masa corporal (IMC). Solicitar al usuario los datos y llamar a la fun-
+ción para mostrar el resultado con dos decimales."""
+"""
+def calcular_imc(peso, altura):
+    imc = peso / (altura**2)
+    return imc
+
+peso_usuario = float(input("Ingrese su peso en kg.\nUtilice punto '.' para decimales: "))
+alt_usuario = float(input("Ingrese su altura.\nUtilice punto '.' para decimales: "))
+print(f"Su índice de masa corporal es: {calcular_imc(peso_usuario, alt_usuario):.2f}--")
+"""
+""""""
