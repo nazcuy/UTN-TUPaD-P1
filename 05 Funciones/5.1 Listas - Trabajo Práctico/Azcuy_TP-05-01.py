d863bdda7c21ef12db9408a9e6861f1dabe36187
Nicolas Azcuy <nico.azcuy@gmail.com>
2025-04-30 23:55:15 -0300

    Subo ejercicio 1

--- 05 Funciones/5.1 Listas - Trabajo Práctico/Azcuy_TP-05-01.py
+++ 05 Funciones/5.1 Listas - Trabajo Práctico/Azcuy_TP-05-01.py
@@ -3,3 +3,9 @@
 
 """ 1) Crear una lista con los números del 1 al 100 que sean múltiplos de 4. 
 Utilizar la función range."""
+"""
+print("Múltiplos de 4, del 1 al 100: ")
+multip = list(range(4, 101, 4))
+print(multip)
+"""
+""""""
