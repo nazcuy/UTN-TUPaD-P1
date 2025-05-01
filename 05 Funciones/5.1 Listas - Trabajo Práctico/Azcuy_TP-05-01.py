b2ec3d6c7d45909a87f11c6a53e337480d0a0e80
Nicolas Azcuy <nico.azcuy@gmail.com>
2025-05-01 00:05:40 -0300

    Subo ejercicio 2.

--- 05 Funciones/5.1 Listas - Trabajo Práctico/Azcuy_TP-05-01.py
+++ 05 Funciones/5.1 Listas - Trabajo Práctico/Azcuy_TP-05-01.py
@@ -8,4 +8,14 @@ print("Múltiplos de 4, del 1 al 100: ")
 multip = list(range(4, 101, 4))
 print(multip)
 """
-""""""
+"""2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) 
+y mostrar el penúltimo. ¡Puedes hacerlo como se muestra en los videos 
+o bien investigar cómo funciona el indexing con números negativos!"""
+"""
+elementos = ["pelota", "café", "destornillador", "computadora", "lapicera"]
+penultimo = elementos[-2]
+print(elementos)
+print(f"Usando indexación negativa, el penúltimo es: {penultimo}")
+penultimo =elementos[3]
+print(f"Usando indexación positiva, el penúltimo es: {penultimo}")
+"""
