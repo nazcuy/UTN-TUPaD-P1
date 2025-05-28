d00fc8fa890d5ce17c8653060385fb6b3101790c
Nicolas Azcuy <nico.azcuy@gmail.com>
2025-05-28 16:27:48 -0300

    Correcciones finales. Trabajo terminado, listo para entregar.

--- 11 Recursividad/Azcuy_TP-11.py
+++ 11 Recursividad/Azcuy_TP-11.py
@@ -3,7 +3,6 @@
 función para calcular y mostrar en pantalla el factorial de todos los números enteros
 entre 1 y el número que indique el usuario
 """
-"""
 def factorial(num):
     if num == 0:
         return 1
@@ -14,10 +13,11 @@ num_usuario = int(input("Ingrese un número: "))
 print(f"Los factoriales desde el 1 hasta el {num_usuario} son:\n")
 for i in range(1, num_usuario + 1):
     print(f"Factorial de {i}: {factorial(i)}")
-"""
+
+
 """2) Crea una función recursiva que calcule el valor de la serie de Fibonacci en la posición
 indicada. Posteriormente, muestra la serie completa hasta la posición que el usuario especifique."""
-"""
+
 def fibonacci(f):
     if f == 0 or f == 1:
         return f
@@ -27,10 +27,10 @@ def fibonacci(f):
 pos = int(input("Ingrese una posición: "))
 for i in range(pos+1):
     print(f"Posición {i}: {fibonacci(i)}")
-"""
+
+
 """3) Crea una función recursiva que calcule la potencia de un número base elevado a un
 exponente, utilizando la fórmula 𝑛^𝑚 = 𝑛 ∗ 𝑚^(𝑚−1). Prueba esta función en un algoritmo general.
-"""
 """
 def potencia(n, m):
     if m == 0:
@@ -43,10 +43,10 @@ base = int(input("Número base: "))
 exp = int(input("Elevado a: "))
 resultado = potencia(base, exp)
 print(f"El resultado es {resultado}.")
-"""
+
+
 """4) Crear una función recursiva en Python que reciba un número entero positivo en base
 decimal y devuelva su representación en binario como una cadena de texto. """
-"""
 def binario(nro):
     if nro == 0:
         return '0'
@@ -57,13 +57,13 @@ def binario(nro):
     
 base10 = int(input("Ingrese un número: "))
 print(f"El número en binario es: {binario(base10)} ")
-"""
+
+
 """5) Implementá una función recursiva llamada es_palindromo(palabra) que reciba una
 cadena de texto sin espacios ni tildes, y devuelva True si es un palíndromo o False si no lo es.
 Requisitos:
 La solución debe ser recursiva.
 No se debe usar [::-1] ni la función reversed()."""
-"""
 def es_palindromo(palabra):
     if len(palabra) == 0:
         return True
@@ -79,7 +79,8 @@ if es_palindromo(frase) == True:
     print("Es un palíndromo!")
 else:
     print("No lo es...")    
-"""
+
+
 """6) Escribí una función recursiva en Python llamada suma_digitos(n) que reciba un
 número entero positivo y devuelva la suma de todos sus dígitos.
 Restricciones:
@@ -89,7 +90,6 @@ Ejemplos:
 suma_digitos(1234) → 10 (1 + 2 + 3 + 4)
 suma_digitos(9) → 9
 suma_digitos(305) → 8 (3 + 0 + 5)"""
-"""
 def suma_digitos(n):
     if n == 0:
         return 0
@@ -98,7 +98,8 @@ def suma_digitos(n):
 
 numero = int(input("Ingrese un número: "))
 print(f"La suma de todos los dígitos es: {suma_digitos(numero)}")
-"""
+
+
 """7) Un niño está construyendo una pirámide con bloques. En el nivel más bajo coloca n
 bloques, en el siguiente nivel uno menos (n - 1), y así sucesivamente hasta llegar al
 último nivel con un solo bloque.
@@ -109,7 +110,6 @@ Ejemplos:
 contar_bloques(1) → 1 (1)
 contar_bloques(2) → 3 (2 + 1)
 contar_bloques(4) → 10 (4 + 3 + 2 + 1)"""
-"""
 def contar_bloques(n):
     if n == 1:
         return 1
@@ -118,7 +118,7 @@ def contar_bloques(n):
     
 bloques= int(input("Ingrese el número de bloques del nivel más bajo: "))
 print(f"La cantidad de bloques que necesita para construir la pirámide son {contar_bloques(bloques)}")
-"""
+
 
 """8) Escribí una función recursiva llamada contar_digito(numero, digito) que reciba un número entero 
 positivo (numero) y un dígito (entre 0 y 9), y devuelva cuántas veces aparece ese dígito dentro del número.
@@ -126,7 +126,6 @@ Ejemplos:
 contar_digito(12233421, 2) → 3
 contar_digito(5555, 5) → 4
 contar_digito(123456, 7) → 0"""
-
 def contar_digito(numero, digito):
     if numero == 0:
         return 0
